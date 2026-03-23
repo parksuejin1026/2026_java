30ac01280641ffec50b867c444526e85f6a359cd
parksuejin1026 <suejin1026suejin@gmail.com>
2026-03-23 15:04:28 +0900

    마법사 추가

--- Study/src/RaidGame/RaidManager.java
+++ Study/src/RaidGame/RaidManager.java
@@ -5,28 +5,29 @@ import java.util.ArrayList;
 public class RaidManager {
 	public static void main(String[] args) {
 		Boss dragon = new Boss("블랙 드래곤", 500, 30);
-		ArrayList<Unit> party = new ArrayList<> ();
-		
+		ArrayList<Unit> party = new ArrayList<>();
+
 		party.add(new Warrior("버서커", 200, 25));
 		party.add(new Warrior("워로드", 250, 15));
-		party.add(new Mage("아이샤" 400, 50));
-		
+		party.add(new Mage("아이샤", 400, 50));
+
 		System.out.println("=== 레이드 시작 ===");
-		
+
 		while (dragon.isAlive() && !party.isEmpty()) {
 			// 1. 영웅들의 턴
-			for (int i = 0 ; i < party.size() ; i++) {
-				Warrior w = party.get(i);
-				w.useSkill(dragon); // 전원 스킬 사용
-				
-				if (!dragon.isAlive()) break;
+			for (int i = 0; i < party.size(); i++) {
+				SkillUsable hero = (SkillUsable) party.get(i);
+				hero.useSkill(dragon); // 전원 스킬 사용
+
+				if (!dragon.isAlive())
+					break;
 			}
-			
+
 			// 2. 보스의 반격 (광역기)
 			if (dragon.isAlive()) {
 				dragon.wideAttack(party);
 			}
-			
+
 			// 3. 사망한 파티원 제거
 			for (int i = 0; i < party.size(); i++) {
 				if (!party.get(i).isAlive()) {
@@ -36,9 +37,11 @@ public class RaidManager {
 				}
 			}
 		}
-		
+
 		System.out.println("=== 전투 종료 ===");
-		if(dragon.isAlive()) System.out.println("레이드 실패....");
-		else System.out.println("축하합니다! 보스를 처지했습니다.");
+		if (dragon.isAlive())
+			System.out.println("레이드 실패....");
+		else
+			System.out.println("축하합니다! 보스를 처지했습니다.");
 	}
 }
